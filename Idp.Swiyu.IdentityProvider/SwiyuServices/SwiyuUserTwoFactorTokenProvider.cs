267312a7a17f7390577e76b3e193b7a0e6e43d97
damienbod <damien_bod@hotmail.com>
2025-10-25 22:26:13 +0200

    Add swiyu 2FA

---
+++ Idp.Swiyu.IdentityProvider/SwiyuServices/SwiyuUserTwoFactorTokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Idp.Swiyu.IdentityProvider.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Idp.Swiyu.IdentityProvider.SwiyuServices;
+
+public class SwiyuUserTwoFactorTokenProvider : IUserTwoFactorTokenProvider<ApplicationUser>
+{
+    public Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<ApplicationUser> manager, ApplicationUser user)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task<string> GenerateAsync(string purpose, UserManager<ApplicationUser> manager, ApplicationUser user)
+    {
+        return Task.FromResult("swiyu");
+    }
+
+    public Task<bool> ValidateAsync(string purpose, string token, UserManager<ApplicationUser> manager, ApplicationUser user)
+    {
+        return Task.FromResult(true);
+    }
+}
