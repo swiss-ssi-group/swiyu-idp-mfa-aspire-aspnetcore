3d117d2a728a21d9524ff20f5e799100232f4dd9
damienbod <damien_bod@hotmail.com>
2025-11-11 21:24:50 +0100

    Fix breaking changes

--- Idp.Swiyu.IdentityProvider/Pages/Diagnostics/ViewModel.cs
+++ Idp.Swiyu.IdentityProvider/Pages/Diagnostics/ViewModel.cs
@@ -1,5 +1,6 @@
 using Duende.IdentityModel;
 using Microsoft.AspNetCore.Authentication;
+using System.Buffers.Text;
 using System.Text;
 using System.Text.Json;
 
@@ -15,7 +16,7 @@ public class ViewModel
         {
             if (encoded != null)
             {
-                var bytes = Base64Url.Decode(encoded);
+                var bytes = Base64Url.DecodeFromChars(encoded);
                 var value = Encoding.UTF8.GetString(bytes);
                 Clients = JsonSerializer.Deserialize<string[]>(value) ?? Enumerable.Empty<string>();
                 return;
