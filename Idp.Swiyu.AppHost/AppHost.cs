4f96302e01657c3158dbc6cb49877cfceda460a9
damienbod <damien_bod@hotmail.com>
2026-09-16 21:23:23 +0200

    Update settings

--- Idp.Swiyu.AppHost/AppHost.cs
+++ Idp.Swiyu.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var IDENTITY_PROVIDER = "identityProvider";
@@ -28,16 +30,21 @@ var verifierOpenIdClientMetaDataFile = builder.AddParameter("verifieropenidclien
 var verifierDid = builder.AddParameter("verifierdid");
 var didVerifierMethod = builder.AddParameter("didverifiermethod");
 var verifierName = builder.AddParameter("verifiername");
-var verifierSigningKey = builder.AddParameter("verifiersigningkey", true);
+var verifierSigningKeyBase64 = builder.AddParameter("verifiersigningkeybase64", secret: true);
+
+var idpWellKnownEndpoint = builder.AddParameter("idpwellknownendpoint");
+var idpJwksUri = builder.AddParameter("idpjwksuri");
+var swiyuMgmtJwtIssuer = builder.AddParameter("verifierjwtissuer");
+
+var verifierSigningKeyBase64Value = await verifierSigningKeyBase64.Resource.GetValueAsync(default);
+var verifierSigningKey = Encoding.UTF8.GetString(Convert.FromBase64String((verifierSigningKeyBase64Value ?? string.Empty)));
 
 /////////////////////////////////////////////////////////////////
 // Verifier OpenID Endpoint: Must be deployed to a public URL
 /////////////////////////////////////////////////////////////////
-// Verifier Management Endpoint: TODO Add JWT security verifier
-// Add security to management API, disabled
 // https://github.com/swiyu-admin-ch/swiyu-verifier?tab=readme-ov-file#security
 /////////////////////////////////////////////////////////////////
-swiyuVerifier = builder.AddContainer("swiyu-verifier", "ghcr.io/swiyu-admin-ch/swiyu-verifier", "latest")
+swiyuVerifier = builder.AddContainer("swiyu-verifier", "ghcr.io/swiyu-admin-ch/swiyu-verifier", "4.2.0")
     .WithEnvironment("EXTERNAL_URL", verifierExternalUrl)
     .WithEnvironment("OPENID_CLIENT_METADATA_FILE", verifierOpenIdClientMetaDataFile)
     .WithEnvironment("VERIFIER_DID", verifierDid)
@@ -47,6 +54,8 @@ swiyuVerifier = builder.AddContainer("swiyu-verifier", "ghcr.io/swiyu-admin-ch/s
     .WithEnvironment("POSTGRES_PASSWORD", postGresPassword)
     .WithEnvironment("POSTGRES_DB", postGresDbVerifier)
     .WithEnvironment("POSTGRES_JDBC", postGresJdbcVerifier)
+    .WithEnvironment("SPRING_SECURITY_OAUTH2_RESOURCESERVER_JWT_ISSUERURI", swiyuMgmtJwtIssuer)
+    .WithEnvironment("VERIFICATION_EXPIRY_MUST_BE_PRESENT", "false")
     .WithHttpEndpoint(port: 8084, targetPort: 8080, name: HTTP);  // local development
     //.WithHttpEndpoint(port: 80, targetPort: 8080, name: HTTP); // for deployment 
 
